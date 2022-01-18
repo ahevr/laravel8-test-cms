dce599204f079fb59a8d276dbdc8132c07c506c1
ahevr <ahmethusreversen@hotmail.com>
2022-01-18 15:03:02 +0300

    ürün detay sayfası eklendi

--- app/Http/Controllers/Site/Search.php
+++ app/Http/Controllers/Site/Search.php
@@ -7,24 +7,13 @@ use App\Models\Admin\UrunlerModel;
 use Illuminate\Http\Request;
 
 class Search extends Controller{
-
     public function index(){
-
         if (strip_tags(trim($_GET["keyword"]!=""))){
-
             $keyword = strip_tags(trim($_GET["keyword"]));
-
             $urunleriGetir = UrunlerModel::where("title","like","%".$keyword."%")->paginate(5);
-
             return view("tema.site.page.homepage.index",compact("urunleriGetir"));
-
         } else {
-
             return redirect("/")->with("toast_warning","Lütfen Bir Arama Yapınız");
-
         }
-
     }
-
-
 }
--- resources/views/tema/site/page/homepage/index.blade.php
+++ resources/views/tema/site/page/homepage/index.blade.php
@@ -1,4 +1,5 @@
 @extends("tema.siteMaster")
+@section('title', "Ürünler")
 @section("content")
     <div class="container">
         <div class="row">
