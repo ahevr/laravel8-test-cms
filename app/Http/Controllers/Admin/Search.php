e310a63151c073d5e29eae5a84dd4e461bf0f520
ahevr <ahmethusreversen@hotmail.com>
2022-01-31 18:15:11 +0300

    admin paneli arama kutusu düzenlendi hatalar giderildi

--- app/Http/Controllers/Admin/Search.php
+++ app/Http/Controllers/Admin/Search.php
@@ -3,20 +3,30 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Admin\AyalarModel;
 use App\Models\Admin\UrunlerModel;
 use Illuminate\Http\Request;
 
-class Search extends Controller
-{
+class Search extends Controller{
+
     public function index(){
 
-        if (strip_tags(trim($_GET["keyword"]!=""))){
+        if (strip_tags(trim(\request("keyword")!=""))){
 
-            $keyword = strip_tags(trim($_GET["keyword"]));
+            $ayar = AyalarModel::all()->first();
+
+            $keyword        = strip_tags(trim(\request("keyword")));
 
             $data = UrunlerModel::where("title","like","%".$keyword."%")->paginate(5);
 
-            return view("tema.admin.page.urunler.index",compact("data"));
+            if (count($data) > 0){
+
+                return view("tema.admin.page.urunler.index",compact("data","ayar"));
+
+            } else {
+
+                return view("errors.404");
+            }
 
         } else {
 
